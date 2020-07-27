1de464ccf969d9159ee78af2602f636d7616a5ef
aang114 <>
2020-07-27 17:44:11 +0800

    Fixed the directory string format where the text files are saved to

---
+++ scripts/add_failed_dates_to_raw_cases_csv.py
@@ -0,0 +1,19 @@
+from common.paths import File, Folder
+import json
+from modules.convert_html_to_txt import ConvertHTMLToTxt
+
+with open (File.url_of_failed_dates_json, 'r') as f:
+    failed_dates_to_urls = json.load(f)
+
+failed_dates = list(failed_dates_to_urls.keys())
+urls = list(failed_dates_to_urls.values())
+
+converter = ConvertHTMLToTxt()
+converter.convert(urls, failed_dates, Folder.text_files_folder_for_failed_dates.replace(' ', '\ '))
+
+
+
+
+
+
+
