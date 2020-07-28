37d29c7dbaf95dab8add413baa640daf14f33213
aang114 <>
2020-07-28 13:56:21 +0800

    Created graph of linked COVID-19 cases in Singapore

--- scripts/create_graph_of_linked_cases.py
+++ scripts/create_graph_of_linked_cases.py
@@ -3,6 +3,7 @@ import pandas as pd
 from common.paths import File
 import re
 import numpy as np
+import matplotlib.pyplot as plt
 
 def return_linked_cases(x):
 
@@ -26,10 +27,26 @@ links_df = aggregated_cases[['Case Number', 'Links']]
 
 links_df['List of Linked Cases'] = links_df['Links'].map(return_linked_cases)
 
-
+links_df.drop(columns=['Links'], inplace=True)
 
 G = nx.Graph()
 
-list_of_edges = []
+edges = []
+
+for case, links in links_df.values:
+    if links is not np.nan:
+        for link in links:
+            edge = (case, link)
+            edges.append(edge)
+
+
+G.add_edges_from(edges)
+
+nx.draw(G, with_labels=True)
+
+#plt.show()
+plt.savefig('../linked_cases.png')
+
+
 
 
